315e8497ba746eabb4bda4499ea7fe090118684c
smj-edison <stephen.mason.jones@gmail.com>
2021-12-15 20:24:48 -0800

    Window debugging

--- recorder/desktop/x11/src/main.rs
+++ recorder/desktop/x11/src/main.rs
@@ -1,8 +1,9 @@
 use libc::c_char;
 use std::ffi::CStr;
 use std::str;
+use std::iter::repeat;
 
-use x11rb::protocol::xproto::{query_tree, get_geometry, list_properties, get_property, Window};
+use x11rb::protocol::xproto::{query_tree, get_geometry, list_properties, get_property, Window, Atom};
 use x11rb::protocol::xproto::AtomEnum;
 use x11rb::rust_connection::RustConnection;
 use x11rb::connection::{Connection, RequestConnection};
@@ -30,27 +31,42 @@ where Conn: RequestConnection + ?Sized {
     Ok(Some(String::from(str_no_terminator)))
 }
 
+fn debug_window_info<Conn>(conn: &Conn, window: Window, depth: Option<usize>) -> Result<(), Box<dyn std::error::Error>>
+where Conn: RequestConnection + ?Sized {
+    let reply = query_tree(conn, window)?.reply()?;
+
+    if !reply.children.is_empty() {
+        println!("children: {}", reply.children.len());
+    }
+
+    for child_window in reply.children {
+        let repeat_string = repeat(">>    ").take(depth.unwrap_or_else(|| 0)).collect::<String>();
+
+        // TODO: are all child windows themselves drawable?
+        let window = get_geometry(conn, child_window)?.reply()?;
+        let atoms = list_properties(conn, child_window)?.reply()?.atoms;        
+
+        //println!("available atoms: {:?}", atoms.iter().map(|&x| AtomEnum::from(x as u8)).collect::<Vec<AtomEnum>>());
+        println!(
+            "{}Window name: {:?}, class: {:?}, command: {:?}",
+            repeat_string,
+            get_window_string(conn, child_window, AtomEnum::WM_NAME)?,
+            get_window_string(conn, child_window, AtomEnum::WM_CLASS)?,
+            get_window_string(conn, child_window, AtomEnum::WM_COMMAND)?,
+        );
+
+        debug_window_info(conn, child_window, Some(depth.unwrap_or_else(|| 0) + 1))?;
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn, screen_num) = RustConnection::connect(None)?;
 
     let screen = &conn.setup().roots[screen_num];
     
-    let reply = query_tree(&conn, screen.root)?.reply()?;
-
-    let blank = String::from("");
-
-    for child_window in reply.children {
-        // TODO: are all child windows themselves drawable?
-        let window = get_geometry(&conn, child_window)?.reply()?;
-        //let atoms = list_properties(&conn, child_window)?.reply()?.atoms;        
-
-        println!(
-            "Window name: {:?}, class: {:?}, command: {:?}",
-            get_window_string(&conn, child_window, AtomEnum::WM_NAME)?,
-            get_window_string(&conn, child_window, AtomEnum::WM_CLASS)?,
-            get_window_string(&conn, child_window, AtomEnum::WM_COMMAND)?
-        );
-    }
+    debug_window_info(&conn, screen.root, None)?;
 
     Ok(())
 }
