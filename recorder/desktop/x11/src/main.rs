89be933dce8ce4d7bc5f50dcec98003df43b1af2
smj-edison <stephen.mason.jones@gmail.com>
2021-12-16 16:53:39 -0800

    More tinkering

--- recorder/desktop/x11/src/main.rs
+++ recorder/desktop/x11/src/main.rs
@@ -26,13 +26,43 @@ fn parse_c_string(chars: &Vec<u8>) -> String {
     String::from(str_no_terminator)
 }
 
-fn get_window_string<Conn>(
+fn get_window_value<Conn, A>(
     conn: &Conn,
     window: Window,
-    string_to_get: AtomEnum,
+    value_to_get: A,
+    value_type: u32,
+    value_length: u32
+) -> Result<Option<Vec<u8>>, Box<dyn std::error::Error>>
+where
+    Conn: RequestConnection + ?Sized,
+    A: Into<Atom>
+{
+    let window_prop = get_property(
+        conn,
+        false,
+        window,
+        value_to_get,
+        value_type,
+        0,
+        value_length,
+    )?
+    .reply()?;
+
+    if window_prop.type_ != u32::from(value_type) {
+        return Ok(None);
+    }
+
+    Ok(Some(window_prop.value))
+}
+
+fn get_window_string_ascii<Conn, A>(
+    conn: &Conn,
+    window: Window,
+    string_to_get: A,
 ) -> Result<Option<String>, Box<dyn std::error::Error>>
 where
     Conn: RequestConnection + ?Sized,
+    A: Into<Atom>
 {
     let window_prop = get_property(
         conn,
@@ -48,8 +78,6 @@ where
     if window_prop.type_ != u32::from(AtomEnum::STRING) {
         return Ok(None);
     }
-
-    
 
     Ok(Some(parse_c_string(&window_prop.value)))
 }
@@ -80,25 +108,33 @@ where
 
         for atom in atoms {
             let name_raw = get_atom_name(conn, atom)?.reply()?.name;
-            let name = parse_c_string(&name_raw);
+            let name = String::from(atom.to_string()) + ": " + &parse_c_string(&name_raw);
 
             atom_names.push(name);
         }
 
         println!(
+            "{}Window name: \x1B[1;32m{:?}\x1B[0m ({:?}), class: {:?} ({:?}), command: {:?} ({:?})",
+            repeat_string,
+            if let Some(name) = get_window_value(conn, child_window, 365 as u32, 350 as u32, 1024)? {
+                String::from(str::from_utf8(&name)?)
+            } else {
+                String::from("")
+            },
+            /*get_window_string(conn, child_window, 39 as u32)?,*/ u32::from(AtomEnum::WM_NAME),
+            get_window_string_ascii(conn, child_window, AtomEnum::WM_CLASS)?, u32::from(AtomEnum::WM_CLASS),
+            get_window_string_ascii(conn, child_window, AtomEnum::WM_COMMAND)?, u32::from(AtomEnum::WM_COMMAND)
+        );
+
+        println!(
             "{}available atoms: {:?}",
             repeat_string,
             atom_names
-        );
-        println!(
-            "{}Window name: {:?}, class: {:?}, command: {:?}",
-            repeat_string,
-            get_window_string(conn, child_window, AtomEnum::WM_NAME)?,
-            get_window_string(conn, child_window, AtomEnum::WM_CLASS)?,
-            get_window_string(conn, child_window, AtomEnum::WM_COMMAND)?,
-        );
+        );        
 
         debug_window_info(conn, child_window, Some(depth.unwrap_or_else(|| 0) + 1))?;
+
+        println!();
     }
 
     Ok(())
@@ -108,6 +144,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn, screen_num) = RustConnection::connect(None)?;
 
     let screen = &conn.setup().roots[screen_num];
+
+    for atom in 0..1000 {
+        let name_raw = match get_atom_name(&conn, atom) {
+            Ok(result) =>  match result.reply() {
+                Ok(result) => result.name,
+                Err(_) => continue
+            },
+            Err(_) => continue
+        };
+
+        let name = String::from(atom.to_string()) + ": " + &parse_c_string(&name_raw);
+
+        //println!("{}", name);
+    }
 
     debug_window_info(&conn, screen.root, None)?;
 
